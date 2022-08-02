b28fcada0b917e6a786e7f3ab6ab8e8579b0a12b
Emre Tunc <emresidar21@outlook.com>
2022-08-02 16:21:05 +0300

    Cosine ve Histogram fonksiyonları eklendi

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from difPy import dif
 import pandas as pd
 import sys
 import os
+import glob
+from PIL import Image
+import numpy as np
+from scipy import spatial
 
 
 class difpy_mod(object):
@@ -37,6 +41,94 @@ class difpy_mod(object):
             if not os.path.exists(directory):
                 os.mkdir(directory)
             shutil.move(path, directory)
+
+    def cosine(self):
+        paths = glob.glob(self.location + "/*")  # photos path
+
+        final_weights = {}
+
+        for i in range(50):
+            weights = []
+            img1 = Image.open(paths[i])
+            img1_reshape = img1.resize((round(img1.size[0] * 0.5), round(img1.size[1] * 0.5)))
+            img_array1 = np.array(img1_reshape)
+            img_array1 = img_array1.flatten()
+            img_array1 = img_array1 / 255
+            for j in range(i + 1, 50):
+                img2 = Image.open(paths[j])
+                img2_reshape = img2.resize((round(img1.size[0] * 0.5), round(img1.size[1] * 0.5)))
+                img_array2 = np.array(img2_reshape)
+                img_array2 = img_array2.flatten()
+                img_array2 = img_array2 / 255
+
+                similarity = -1 * (spatial.distance.cosine(img_array1, img_array2) - 1)
+                if similarity >= 0.94:
+                    weights.append([similarity, paths[j]])
+            print(i)
+            if weights != []:
+                final_weights[paths[i]] = weights
+
+        files = []
+        for i in list(final_weights.values()):
+            for j in i:
+                files.append(j[1])
+
+        files = set(files)
+        for file in files:
+            path = file
+            if not os.path.exists(self.destination):
+                os.mkdir(self.destination)
+            shutil.move(path, self.destination)
+
+    def histogram(self):
+        def make_regalur_image(img, size=(64, 64)):
+            gray_image = img.resize(size).convert('RGB')
+            return gray_image
+
+        # Calculate histogram
+        def hist_similar(lh, rh):
+            assert len(lh) == len(rh)
+            hist = sum(1 - (0 if l == r else float(abs(l - r)) / max(l, r)) for l, r in zip(lh, rh)) / len(lh)
+            return hist
+
+        # Calculate similarity
+        def calc_similar(li, ri):
+            calc_sim = hist_similar(li.histogram(), ri.histogram())
+            return calc_sim
+
+        paths = glob.glob(self.location + "/*")  # photos path
+        final_weights = {}
+        for i in range(50):
+            weights = []
+            image1 = Image.open(paths[i])
+            image1 = make_regalur_image(image1)
+            for j in range(i + 1, 50):
+                image2 = Image.open(paths[j])
+                image2 = make_regalur_image(image2)
+                # print("The similarity between pictures is", calc_similar(image1, image2))
+                similarity = calc_similar(image1, image2)
+                # print(similarity)
+                if similarity >= 0.4:
+                    weights.append([similarity, paths[j]])
+            print(i)
+            if weights != []:
+                final_weights[paths[i]] = weights
+
+        files = []
+
+        for i in final_weights.values():
+            for j in i:
+                files.append(j[1])
+
+        files = set(files)
+
+        for file in files:
+            path = file
+            if not os.path.exists(self.destination):
+                os.mkdir(self.destination)
+            shutil.move(path, self.destination)
+
+
 # move
 # delete
 # threshold
@@ -48,9 +140,11 @@ def main():
     args = sys.argv[1:]
     if sys.argv[1] == "method1":
         a = difpy_mod(args)
-        #a.searched()
-        a.move()
-        #a.delete()
+        # a.searched()
+        # a.move()
+        # a.delete()
+        # a.cosine()
+        a.histogram()
     else:
         print("There is no library such as,", sys.argv[1])
 
