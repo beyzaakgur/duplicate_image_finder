507eac1374f8c05fdf9dc1d315aa5abdb17d28f5
Beyza Akgür <beyzaakgur@gmail.com>
2022-08-02 11:36:20 +0300

    move ve delete fonksiyonları oluşturuldu.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from difPy import dif
 import pandas as pd
 import sys
 import os
-import imagehash
 
 
 class difpy_mod(object):
@@ -20,18 +19,38 @@ class difpy_mod(object):
             files.append(i[len(self.location) + 1:])
         return [search, files]
 
+    def move(self):
+        search = self.searched()
+        for file in search[1]:
+            path = os.path.join(self.location, file)
+            if not os.path.exists(self.destination):
+                os.mkdir(self.destination)
+            shutil.move(path, self.destination)
 
+    def delete(self):
+        search = self.searched()
+        for file in search[1]:
+            path = os.path.join(self.location, file)
+            directory = self.location + "_del"
+            parent_dir = os.getcwd()
+            directory = os.path.join(parent_dir, directory)
+            if not os.path.exists(directory):
+                os.mkdir(directory)
+            shutil.move(path, directory)
 # move
 # delete
 # threshold
 # cosine similarity
 # l2 similarity
 
+
 def main():
     args = sys.argv[1:]
     if sys.argv[1] == "method1":
         a = difpy_mod(args)
-        a.searched()
+        #a.searched()
+        a.move()
+        #a.delete()
     else:
         print("There is no library such as,", sys.argv[1])
 
