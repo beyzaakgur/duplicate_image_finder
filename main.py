f86d730a83ad7e6a56fbf415c7087770ab8fd9f7
Emre Tunc <emresidar21@outlook.com>
2022-08-02 13:05:39 +0300

    class yapısı oluşturuldu

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 import sys
 import os
 import imagehash
+from PIL import Image
 
 
 class difpy_mod(object):
@@ -20,6 +21,39 @@ class difpy_mod(object):
             files.append(i[len(self.location) + 1:])
         return [search, files]
 
+    def move(self):
+        search = self.searched()
+        for file in search[1]:
+            path = os.path.join(self.location, file)
+            shutil.move(path, self.destination)
+
+    def delete(self):
+        search = self.searched()
+        for file in files:
+            path = os.path.join(location, file)
+            # Remove the file
+            # 'file.txt'
+            os.remove(path)
+
+
+    ## Convert the picture to RGB
+    # def make_regalur_image(self, img, size=(64, 64)):
+    #    gray_image = img.resize(size).convert('RGB')
+    #    return gray_image
+
+
+#
+## Calculate histogram
+# def hist_similar(self, lh, rh):
+#    assert len(lh) == len(rh)
+#    hist = sum(1 - (0 if l == r else float(abs(l - r)) / max(l, r)) for l, r in zip(lh, rh)) / len(lh)
+#    return hist
+#
+## Calculate similarity
+# def calc_similar(self, li, ri):
+#    calc_sim = self.hist_similar(li.histogram(), ri.histogram())
+#    return calc_sim
+
 
 # move
 # delete
@@ -29,11 +63,18 @@ class difpy_mod(object):
 
 def main():
     args = sys.argv[1:]
-    if sys.argv[1] == "method1":
+    if sys.argv[1] == "difPy":
         a = difpy_mod(args)
-        a.searched()
+        #a.searched()
+        a.move()
     else:
         print("There is no library such as,", sys.argv[1])
+
+    # image1 = Image.open('ilPBHd3r3a.jpg')
+    # image1 = a.make_regalur_image(image1)
+    # image2 = Image.open('Johnny-Depp.jpg')
+    # image2 = a.make_regalur_image(image2)
+    # print("The similarity between pictures is", a.calc_similar(image1, image2))
 
 
 if __name__ == "__main__":
